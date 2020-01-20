19e7c701e3ce7b6ec2a27f0e60a3dc0b925e168e
Julian Camilo <julian20141016@gmail.com>
2020-01-19 19:34:07 -0500

    Login redireccionado Correctamente (excepto para admin)

--- paginaPrincipal/redireccionador.php
+++ paginaPrincipal/redireccionador.php
@@ -2,33 +2,31 @@
 include('../conexion.php');
 include('../php/objetoUsuario.php');
 
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$whatsapp = $_POST['whatsapp'];
-$nequi = $_POST['nequi'];
 $usuario = $_POST['usuario'];
 $clave = $_POST['clave'];
-$claveRepeat = $_POST['claveRepeat'];
-$codigo = $_POST['codigo'];
 
-$usuarioActivo = new usuario($usuario, $clave);
-$usuarioActivo->iniciarSesion();
-
-if ($usuarioActivo->verificarRegistro()=="usuarioRepetido") {
-	header("Location:usuarioRepetido.php");
-} elseif ($usuarioActivo->verificarRegistro()=="correoRepetido") {
-	header("Location:correoRepetido.php");
-} elseif ($usuarioActivo->verificarRegistro()=="correcto" && $clave==$claveRepeat) {
-	if ($codigo="2d7ba3fa7eb") {
-		header("Location:../php/registro/registroExitosoNormal.php");
-	} else {
-		header("Location:../php/registro/registroExitosoColaborador.php");
-	}
-	
-} else {
-	header("Location:claveNoCoincide.php");
+$consulta = "SELECT * FROM `usuarios` WHERE 1";
+$resultado = mysqli_query($conexion, $consulta);
+$credencialesCorrectas = false;
+while ($columna = mysqli_fetch_array( $resultado ))
+{
+	if ($columna['usuario']==$usuario) {
+		if ($columna['clave']==$clave) {
+			$credencialesCorrectas = true;
+			$usuarioActivo = new usuario($usuario, $clave);
+			$usuarioActivo->iniciarSesion();
+			}
+		}
 }
 
-#hay que actualizar este redireccionador, este tiene que dirigir segun el tipo de usuario desde el login
+if(!$credencialesCorrectas){
+	header("Location:errorDeCredenciales.php");
+}
+elseif($usuarioActivo->getTipoUsuario()=="cliente"){
+	header("Location:/cliente/home.php");
+}
+elseif ($usuarioActivo->getTipoUsuario()=="colaborador") {
+	header("Location:/encargado/home.php");
+}
 
-?>
+?> 
